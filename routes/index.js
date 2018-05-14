2223a2bcaa4950d56229634c205d9a46da6d0105
Christian Camacho <xentyomail@gmail.com>
2018-05-14 15:01:10 -0700

    Add embed video

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'X-PARTY' });
 });
+
+router.get('/current-song', function(req, res){
+  res.render('current-song', {
+    title: 'x-party - music',
+    videoURL: 'https://www.youtube.com/embed/sEICvn4VwVg?rel=0&autoplay=1&amp;showinfo=0&vq=hd1080&mute=1'
+  })
+})
 
 module.exports = router;
