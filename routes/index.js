c5cde430779c9d7c51e44729aa65a1a50363c2b6
Christian Camacho <xentyomail@gmail.com>
2018-05-18 13:29:28 -0700

    Add dotenv

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'X-PARTY' });
+  res.render('index', { title: 'x-party' });
 });
 
 router.get('/current-song', function(req, res){
