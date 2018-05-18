faa9f7fd99534601415c840858311d1b76af33ab
Christian Camacho <xentyomail@gmail.com>
2018-05-18 13:41:04 -0700

    change default video

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 router.get('/current-song', function(req, res){
   res.render('current-song', {
     title: 'x-party - music',
-    videoURL: 'https://www.youtube.com/embed/sEICvn4VwVg?rel=0&autoplay=1&amp;showinfo=0&vq=hd1080&mute=1'
+    videoURL: 'https://www.youtube.com/embed/_2DkJjBiCWY?rel=0&autoplay=1&amp;showinfo=0&vq=hd1080&mute=1'
   })
 })
 
